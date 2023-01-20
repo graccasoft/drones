bd378cd2b40159f6be7cfe336b8fb257c4dd7b31
Gracious Mashasha <graccasoft263@gmail.com>
2023-01-20 16:17:55 +0200

    Loading medication unit tests

--- src/test/java/com/graccasoft/drones/controller/DispatchControllerTest.java
+++ src/test/java/com/graccasoft/drones/controller/DispatchControllerTest.java
@@ -2,6 +2,7 @@ package com.graccasoft.drones.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.graccasoft.drones.entity.Drone;
+import com.graccasoft.drones.entity.Medication;
 import com.graccasoft.drones.enums.DroneState;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,6 +11,8 @@ import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
@@ -47,6 +50,59 @@ class DispatchControllerTest {
         this.mockMvc.perform(post("/api/v1/dispatch/register")
                         .contentType(MediaType.APPLICATION_JSON)
                         .content( asJsonString(drone) )
+                )
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    public void shouldThrowAnExceptionWhenLoadingMedicationWithInvalidInput() throws Exception {
+
+        Medication medication = new Medication();
+        medication.setCode("|--!!!");
+        List<Medication> medicationList = new ArrayList<>();
+        medicationList.add(medication);
+
+        this.mockMvc.perform(post("/api/v1/dispatch/load/1")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content( asJsonString(medication) )
+                )
+                .andExpect(status().isBadRequest());
+    }
+
+    @Test
+    public void shouldLoadDroneWhenLoadingMedicationWithValidInput() throws Exception {
+
+        Medication medication = new Medication();
+        medication.setCode("1023902");
+        medication.setName("Paracetamol");
+        List<Medication> medicationList = new ArrayList<>();
+        medicationList.add(medication);
+
+        this.mockMvc.perform(post("/api/v1/dispatch/load/1")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content( asJsonString(medicationList) )
+                )
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    public void shouldThrowExceptionWhenLoadingMedicationHeavierThanDrone() throws Exception {
+
+        Drone drone = new Drone();
+        drone.setState(DroneState.IDLE);
+        drone.setWeightLimit(new BigDecimal(200));
+        drone.setSerialNumber("1002023");
+
+        Medication medication = new Medication();
+        medication.setCode("1023902");
+        medication.setName("Paracetamol");
+        medication.setWeight(new BigDecimal(300));
+        List<Medication> medicationList = new ArrayList<>();
+        medicationList.add(medication);
+
+        this.mockMvc.perform(post("/api/v1/dispatch/load/1")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content( asJsonString(medicationList) )
                 )
                 .andExpect(status().isOk());
     }
