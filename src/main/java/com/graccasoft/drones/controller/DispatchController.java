86b7344e333d45ac1926c41aa02fc28a57852f57
Gracious Mashasha <graccasoft263@gmail.com>
2023-01-20 18:32:08 +0200

    Dispatch Controller implementation using the Service

--- src/main/java/com/graccasoft/drones/controller/DispatchController.java
+++ src/main/java/com/graccasoft/drones/controller/DispatchController.java
@@ -4,6 +4,7 @@ import com.graccasoft.drones.dto.BatteryLevelResponse;
 import com.graccasoft.drones.dto.StandardResponse;
 import com.graccasoft.drones.entity.Drone;
 import com.graccasoft.drones.entity.Medication;
+import com.graccasoft.drones.service.DispatchService;
 import jakarta.validation.Valid;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
@@ -15,34 +16,45 @@ import java.util.List;
  * @author graciousmashasha
  * @created 20/01/2023 - 3:13 pm
  */
-@Controller
+@RestController
 @RequestMapping("/api/v1/dispatch")
 public class DispatchController {
 
-    //register drone endpoint
+    private final DispatchService dispatchService;
 
-    @PostMapping("register")
-    public Drone registerDrone(@Valid @RequestBody Drone drone){
-        return null;
+    //DispatchService dependency will be auto injected by Spring
+    public DispatchController(DispatchService dispatchService) {
+        this.dispatchService = dispatchService;
     }
 
+    //register drone endpoint
+    @PostMapping("register")
+    public Drone registerDrone(@Valid @RequestBody Drone drone){
+        return dispatchService.saveDrone(drone);
+    }
+
+    //load medication to drone endpoint
     @PostMapping("load/{droneId}")
     public StandardResponse loadDrone(@Valid @RequestBody List<Medication> medicationItems, @PathVariable Integer droneId){
+        dispatchService.loadDrone(droneId, medicationItems);
         return new StandardResponse(200,"Drone loaded");
     }
 
+    //fetch medication loaded to drone endpoint
     @GetMapping("loaded/{droneId}")
     public List<Medication> getLoadedMedication(@PathVariable Integer droneId){
-        return null;
+        return dispatchService.getDroneMedicationList(droneId);
     }
 
+    //check available drones
     @GetMapping("/available-drones")
     public List<Drone> getAvailableDrones(){
-        return null;
+        return dispatchService.getAvailableDrones();
     }
 
+    //check drone battery level
     @GetMapping("/batter-level/{droneId}")
     public BatteryLevelResponse getBatteryLevel(@PathVariable Integer droneId){
-        return new BatteryLevelResponse("Battery Level", BigDecimal.ZERO);
+        return dispatchService.getDroneBatteryLevel(droneId);
     }
 }
